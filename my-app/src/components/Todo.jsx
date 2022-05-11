549c08636440c105106059c475c4622e784cd110
Miriam <ebertmiriam@gmx.de>
2022-05-11 11:27:43 +0200

    save project in rep

---
+++ my-app/src/components/Todo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Todo = ({ description, done, onChangeTodo, onDeleteTodo, index }) => {
+
+  return (
+    <div>
+      <div
+        className={
+          done ? "flex justify-between items-center p-2 bg-green-600"
+            : "flex justify-between items-center p-2 bg-red-500"
+        }>
+        <h1 className="text-lg cursor-pointer text-white"
+          onClick={
+            () => onChangeTodo(index)
+          }
+        >{description}</h1>
+        <button className="text-lg bg-gray-400 p-2 text-white"
+        onClick={
+          () => onDeleteTodo(index)
+        }>Löschen</button>
+      </div>
+    </div >
+  )
+}
+
+export default Todo
